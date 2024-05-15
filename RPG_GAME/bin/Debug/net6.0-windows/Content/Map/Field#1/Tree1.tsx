063af2cb852c685bbf227794e5bc1af5c0f768ca
willy <willypoland0@gmail.com>
2024-05-16 00:38:17 +0300

    clean solution

--- RPG_GAME/bin/Debug/net6.0-windows/Content/Map/Field#1/Tree1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tree1" tilewidth="240" tileheight="288" tilecount="1" columns="1">
- <image source="Tree1.png" width="240" height="288"/>
-</tileset>
